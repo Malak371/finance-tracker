6f251a2047a0642bade7e66e940381b11bee930b
Malak371 <113465527+Malak371@users.noreply.github.com>
2025-09-16 17:22:27 -0400

    basic actions done

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoFlush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 import models, database
+from fastapi.middleware.cors import CORSMiddleware
 from routes import auth, expenses, dashboard
 
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],  # allow POST, GET, OPTIONS, etc
+    allow_headers=["*"],  # allow Authorization, Content-Type, etc
+)
 
 app.include_router(auth.router)
 app.include_router(expenses.router)
